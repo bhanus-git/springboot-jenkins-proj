8aa2633b4f836aac4ece181a974f1208505e0c01
bhanus-git <bhanusbc@hotmail.com>
2024-10-17 14:04:34 +0530

    forcefully build fails

--- src/test/java/com/example/springboot_jenkins_proj/SpringbootJenkinsProjApplicationTests.java
+++ src/test/java/com/example/springboot_jenkins_proj/SpringbootJenkinsProjApplicationTests.java
@@ -14,7 +14,7 @@ class SpringbootJenkinsProjApplicationTests {
 	public static Logger logger = LoggerFactory.getLogger(SpringbootJenkinsProjApplication.class);
 
 	@Test
-	void contextLoads() {
+	 contextLoads() {
 		logger.info("Test case executing...");
 		logger.info("Test case executing second log statement...");
 		assertEquals(true,true);
